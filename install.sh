8cc7e8bea48927812b358a6b752d3dc12de6ebea
Adam <a.halesworth@gmail.com>
2013-11-27 10:52:45 +0000

    Changed variable name for clarity

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "This script will install the Jellybeans colorscheme for Xcode 4."
+echo "This script will install the Jellybeans colorscheme for Xcode 4 and up."
 echo "Do you wish to continue? y/n: "
-read name
-if [ $name == 'y' ]; then
+read decision
+if [ $decision == 'y' ]; then
 	echo "Copying to ~/Library/Developer/Xcode/UserData/FontAndColorThemes"
 	cp Jellybeans.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
 	echo "Complete..."
