5562d675e6b164cc67c08d374182ecdc92597373
Richo Healey <richo@psych0tik.net>
2014-10-22 13:24:06 -0700

    Create the FontAndColorThemes directory if it's not there

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+set -e
+
+TARGET=~/Library/Developer/Xcode/UserData/FontAndColorThemes
 
 echo "This script will install the Jellybeans colorscheme for Xcode 4 and up."
 echo "Do you wish to continue? y/n: "
 read decision
 if [ $decision == 'y' ]; then
-	echo "Copying to ~/Library/Developer/Xcode/UserData/FontAndColorThemes"
-	cp Jellybeans.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
+	if [ ! -d "$TARGET" ]; then
+		echo "Creating $TARGET"
+		mkdir -p "$TARGET"
+	fi
+	echo "Copying to $TARGET"
+	cp Jellybeans.dvtcolortheme "$TARGET"
 	echo "Complete..."
 else
 	exit
